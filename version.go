8d5f24c374f9b9a69bcde7697754b431995d8019
theboringhumane <me@iamharsh.dev>
2025-12-22 15:32:23 +0530

    Add versioning support to D1 Go SQL Driver by introducing version.go file with version constant set to "0.0.2".

---
+++ version.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2025 SyneHQ. All rights reserved.
+// Use of this source code is governed by the MIT License that can be
+// found in the LICENSE file.
+
+package d1sql
+
+const Version = "0.0.2"
